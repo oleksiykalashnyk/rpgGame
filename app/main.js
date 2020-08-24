4604ced6531d15dc7c6fb111c17d1b602c1d0e2e
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-08-24 18:59:38 +0200

    add new fix

--- app/main.js
+++ app/main.js
@@ -1,19 +1,15 @@
 "use strict";
 
-const Hero = {
-        hp: 0,
-        armor: 0,
-        agility: 0,
-        magic: 0,
-        power: 0,
-        luck: 0
-    },
+const
+
+    // Объекты для хранения данных состояния
     archer = {},
     wizard = {},
     warrior = {},
+
+    //Характеристики героев!!!
     setHero = {
         setArcher: () => {
-            Object.setPrototypeOf(archer, Hero);
             archer.name = "Legolas";
             archer.type = "archer";
             archer.hp = 350;
@@ -24,7 +20,6 @@ const Hero = {
             archer.luck = 0.258;
         },
         setWizard: () => {
-            Object.setPrototypeOf(wizard, Hero);
             wizard.name = "Gendalf";
             wizard.type = "wizard";
             wizard.hp = 300;
@@ -35,7 +30,6 @@ const Hero = {
             wizard.luck = 0.312;
         },
         setWarrior: () => {
-            Object.setPrototypeOf(warrior, Hero);
             warrior.name = "Aragorn";
             warrior.type = "warrior";
             warrior.hp = 400;
@@ -46,6 +40,8 @@ const Hero = {
             warrior.luck = 0.28;
         }
     },
+
+    //Объект для показа статуса героя
     getHero = {
         getArcher: () => {
             console.log("Status now:");
@@ -76,24 +72,35 @@ const Hero = {
         },
     },
 
-
+    //Случайное число от 9 до 99
     getRandomInt = () => {
-        return Math.floor(Math.random() * (9 - 5)) + 5;
+        return Math.floor(Math.random() * (99 - 9)) + 9;
     },
 
+    //Вычисляем атаку Мечём!!!
     attackSword = (a) => {
         let random = getRandomInt();
-        a.attack = a.power * (((a.agility / random + (a.power / a.luck)) / (random)) / 100);
-    },
-    attackBow = (a) => {
-        a.attack = ((a.power + (a.agility / a.luck)) / (a.luck)) / 100;
-        console.log(a.attack);
-    },
-    attackMagic = (a) => {
-        a.attack = ((a.agility + (a.power / a.luck)) / (a.luck)) / 100;
-        console.log(a.attack);
+        a.attack = (a.power * (random / 100)) - (a.agility / (random * 100));
+        console.log(a.name + " ударил мечём на " + a.attack + " урона");
     },
 
+    //Вычисляем атаку Луком!!!
+    attackBow = (a) => {
+        let random = getRandomInt();
+        a.attack = (a.agility * (random / 100)) - (a.power / (random * 100));
+        console.log(a.name + " пустил стрелу на " + a.attack + " урона");
+    },
+
+    //Вычисляем атаку Волшебством!!!
+    attackMagic = (a) => {
+        let random = getRandomInt();
+        a.attack = a.magic * (random / 100);
+        console.log(a.name + " использовал заклятие  на " + a.attack + " урона");
+    },
+
+
+
+    //Создаём монстров
     m0 = {},
     m1 = {},
     m2 = {},
@@ -105,45 +112,101 @@ const Hero = {
     m8 = {},
     m9 = {},
 
+    //Характеристики монстров
     setMonstr = {
         set0: () => {
-            Object.setPrototypeOf(m0, Hero);
             m0.name = "Уебан";
             m0.type = "monster";
-            m0.hp = 200;
-            m0.armor = 0;
+            m0.hp = 150;
+            m0.armor = 100;
             m0.agility = 20;
             m0.magic = 0;
             m0.power = 40;
             m0.luck = 0.08;
+        },
+        set1: () => {
+            m1.name = "Властелин Пипца";
+            m1.type = "monster";
+            m1.hp = 350;
+            m1.armor = 0;
+            m1.agility = 25;
+            m1.magic = 0;
+            m1.power = 50;
+            m1.luck = 0.08;
         }
     },
+
+    //Да начнёться битва!!!
     startFigth = (a, b) => {
         do {
-            attackSword(a);
-            attackSword(b);
-            console.log(`Ты атакуешь ${b.name} у которого ${b.hp}`);
-            b.hp -= a.attack;            
+            let randomForFigth = getRandomInt();
+            randomForFigth /= 100;
+            console.log("Счастливое число = " + randomForFigth + " твоё счастливое число = " + a.luck);
+
+            if (a.type == "archer") {
+                attackBow(a);
+            }
+            if (a.type == "wizard") {
+                attackMagic(a);
+            }
+            if (a.type == "warrior") {
+                attackSword(a);
+            }
+
+            console.log(`Ты атакуешь ${b.name} у которого ${b.hp}  и ${b.armor} брони`);
+
+            if (randomForFigth >= a.luck) {
+                if (b.armor > 0) {
+                    let countArmor = b.armor - a.attack;
+                    if (countArmor < 0) {
+                        b.armor -= a.attack;
+                        b.hp += countArmor;
+                        console.log(`Тебе не повезло ты ударил в броню ${a.attack} и снёс всю броню!!! Поскольку брони было мало для такого урона был нанесён дополнительный урон в ХП ${(-1)*countArmor}`);
+                        b.armor = 0;
+                    } else {
+                        b.armor -= a.attack;
+                        console.log(`Тебе не повезло ты ударил в броню ${a.attack}`);
+                    }
+
+                } else {
+                    console.log(`Тебе повезло у ${b.name}а нет брони и ударил сквозь броню ${a.attack}`);
+                    b.hp -= a.attack;
+                }
+            } else {
+                console.log(`Тебе повезло ты ударил сквозь броню ${a.attack}`);
+                b.hp -= a.attack;
+            }
+
             console.log(`Наресенно уронна ${a.attack}`);
-            console.log(`У ${b.name}а осталось ${b.hp}`);
+            console.log(`У ${b.name}а осталось ${b.hp} ХП и ${b.armor} брони`);
             if (b.hp <= 0) {
                 break;
-              }
+            }
+            attackSword(b);
             a.hp -= b.attack;
             console.log(`${b.name} Ударил нас на ${b.attack} урона`);
-            console.log(`У тебя осталось ${a.hp}`);
-            console.log("");
+            console.log(`У тебя осталось ${a.hp} ХП`);
+            console.log("------------------------------");
             if (a.hp <= 0) {
                 break;
-              }
-        }while (b.hp > 0);
+            }
+        } while (b.hp >= 0 || a.hp >= 0);
 
         if (a.hp <= 0) {
             console.log(`Ты ПРОИГРАЛ ${a.name}`);
         }
         if (b.hp <= 0) {
-            console.log(`Ты победил ${b.name}`);
+            console.log(`Ты победил ${a.name}!!! ${b.name} - повержен`);
         }
+    },
+
+    //Сброс данных
+    reset = () => {
+        setHero.setArcher();
+        setHero.setWizard();
+        setHero.setWarrior();
+        setMonstr.set0();
+        setMonstr.set1();
     };
 
 
@@ -152,11 +215,36 @@ setHero.setWarrior();
 setHero.setWizard();
 
 setMonstr.set0();
+setMonstr.set1();
 
 attackSword(archer);
 attackSword(wizard);
 attackSword(warrior);
+console.log("------------------------------");
+
+attackMagic(archer);
+attackMagic(wizard);
+attackMagic(warrior);
+console.log("------------------------------");
+
+attackBow(archer);
+attackBow(wizard);
+attackBow(warrior);
+console.log("------------------------------");
+
 attackSword(m0);
+attackSword(m1);
+console.log("------------------------------");
+
+// let x = [];
+// for (let i = 0; i <= 99; i++) {
+//     attackSword(warrior);
+//     x[i] = warrior.attack;
+// }
+// x.sort();
+// console.log(x);
 
 
-startFigth(archer, m0);
+
+//startFigth(archer, m0);
+//startFigth(wizard,m1);
